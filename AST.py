d09f3e299f356e5e9f2ba50de1f3e16277f0e2c6
Bailin Huang <bailin.huang@ucr.ac.cr>
2018-07-02 16:41:21 -0600

    AST

---
+++ AST.py
@@ -0,0 +1,15 @@
+class Metodo:
+    def __init__(self, parameters, size):
+        self.parameters = parameters
+        self.size = size
+
+class Print:
+    def __init__(self, parameter):
+        self.parameter = parameter
+
+class InstructionList:
+    def __init__(self):
+        self.instructions = []
+
+    def addInstructions(self, instruction):
+        self.instructions.append(instruction)
--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 import ply.yacc as yacc
 from lex import tokens
+from AST import *
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -10,22 +11,30 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+ast = InstructionList()
 
 def p_START(p):
     'start : metodo NEWLINE start'
+    ast.addInstructions(p[1])
+    ast.addInstructions(p[3])
 
 def p_START2(p):
     'start : metodo NEWLINE'
+    ast.addInstructions(p[1])
 
 def p_START3(p):
     'start : print NEWLINE start'
+    ast.addInstructions(p[1])
+    ast.addInstructions(p[3])
 
 def p_START4(p):
     'start : print NEWLINE'
+    ast.addInstructions(p[1])
 
 ######### METODO EXAMEN
 def p_METODO(p):
     'metodo : number_assignations ASSIGNATION METODO LEFTPAR INT RIGHTPAR'
+    p[0] = Metodo(p[1], p[5])
 
 def p_METODO_error(p):
     'metodo : number_assignations ASSIGNATION METODO LEFTPAR INT error'
@@ -49,23 +58,29 @@ def p_METODO_error5(p):
 
 def p_NUMBER_ASSIGNATIONS1(p):
     'number_assignations : NAME'
+    p[0] = [p[1]]
 
 def p_NUMBER_ASSIGNATIONS2(p):
     'number_assignations : NAME COMMA NAME'
+    p[0] = [p[1], p[3]]
 
 def p_NUMBER_ASSIGNATIONS3(p):
     'number_assignations : NAME COMMA NAME COMMA NAME'
+    p[0] = [p[1], p[3], p[5]]
 
 def p_NUMBER_ASSIGNATIONS4(p):
     'number_assignations : NAME COMMA NAME COMMA NAME COMMA NAME COMMA'
+    p[0] = [p[1], p[3], p[5], p[7]]
 
 def p_NUMBER_ASSIGNATIONS5(p):
     'number_assignations : NAME COMMA NAME COMMA NAME COMMA NAME COMMA NAME'
+    p[0] = [p[1], p[3], p[5], p[7], p[9]]
 
 
 ###### INSTRUCTIONS
 def p_PRINT(p):
     'print : PRINT LEFTPAR NAME RIGHTPAR'
+    p[0] = Print[3]
 
 def p_PRINT_error(p):
     'print : PRINT LEFTPAR NAME error'
@@ -78,6 +93,7 @@ def p_PRINT_error2(p):
 def p_PRINT_error3(p):
     'print : PRINT error'
     print("Found ", p[2].value, ". Expected (")
+
 # def p_error(p):
 #     if p:
 #         print(bcolors.FAIL+"Error:" +bcolors.ENDC ,bcolors.HEADER + p.type+ bcolors.ENDC, bcolors.BOLD + "", p.value,"" + bcolors.ENDC, bcolors.WARNING + "Sucedió en la línea:" + bcolors.ENDC, bcolors.UNDERLINE + "" ,p.lineno,"" + bcolors.ENDC)
@@ -92,5 +108,7 @@ def parserAnalysis():
         parser.parse(line)
         break
 
+def getAST():
+    return ast
+
 parser = yacc.yacc()
-parserAnalysis()
